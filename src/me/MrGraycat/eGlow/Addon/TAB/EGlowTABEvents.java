06f0c3b82b2f0da522d4de792aea4582ddeda072
MrGraycat <moorsn@gmail.com>
2021-09-05 07:35:36 +0200

    Improving memory usage (flare reports)

--- src/me/MrGraycat/eGlow/Addon/TAB/EGlowTABEvents.java
+++ src/me/MrGraycat/eGlow/Addon/TAB/EGlowTABEvents.java
@@ -6,7 +6,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import me.MrGraycat.eGlow.EGlow;
 import me.MrGraycat.eGlow.API.Event.GlowColorChangeEvent;
@@ -26,52 +25,54 @@ public class EGlowTABEvents implements Listener {
 	
 	@EventHandler
 	public void onColorChange(GlowColorChangeEvent event) {
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-				try {
-					if (getInstance().getTABAddon() != null) {
-						if (getInstance().getTABAddon().installedOnBukkit() && !getInstance().getTABAddon().isUnlimitedNametagModeEnabled()) {
-							cancel();
-							return;
-						}				
+		try {
+			if (getInstance().getTABAddon() != null) {
+				if (getInstance().getTABAddon().installedOnBukkit() && !getInstance().getTABAddon().isUnlimitedNametagModeEnabled()) {
+					//cancel();
+					return;
+				}				
+				
+				if (event.getPlayer() != null) {
+					if (getInstance().getTABAddon().installedOnBukkit() && TABAPI.getPlayer(event.getPlayerUUID()) != null) {
+						TabPlayer tabPlayer = (TabPlayer) TABAPI.getPlayer(event.getPlayerUUID()); 
+						String tagPrefix = "";
+						String color = (event.getChatColor().equals(ChatColor.RESET)) ? "" : event.getColor();
 						
-						if (event.getPlayer() != null) {
-							if (getInstance().getTABAddon().installedOnBukkit() && TABAPI.getPlayer(event.getPlayerUUID()) != null) {
-								TabPlayer tabPlayer = (TabPlayer) TABAPI.getPlayer(event.getPlayerUUID()); 
-								String tagPrefix = "";
-								String color = (event.getChatColor().equals(ChatColor.RESET)) ? "" : event.getColor();
+						try {tagPrefix = tabPlayer.getOriginalValue(EnumProperty.TAGPREFIX);} catch(Exception ex) {tagPrefix = "";}
+						
+						try {
+							if (!EGlowMainConfig.OptionAdvancedTABIntegration()) {
+								tabPlayer.setValueTemporarily(EnumProperty.TAGPREFIX, (!tagPrefix.isEmpty()) ? tagPrefix + color : color);
+							} else {
+								String customTagName = "";
 								
-								try {tagPrefix = tabPlayer.getOriginalValue(EnumProperty.TAGPREFIX);} catch(Exception ex) {tagPrefix = "";}
-								
-								try {
-									if (!EGlowMainConfig.OptionAdvancedTABIntegration()) {
-										tabPlayer.setValueTemporarily(EnumProperty.TAGPREFIX, (!tagPrefix.isEmpty()) ? tagPrefix + color : color);
-									} else {
-										String customTagName = "";
-										
-										try {customTagName = tabPlayer.getOriginalValue(EnumProperty.CUSTOMTAGNAME);} catch(Exception ex) {customTagName = event.getPlayer().getName();}
+								try {customTagName = tabPlayer.getOriginalValue(EnumProperty.CUSTOMTAGNAME);} catch(Exception ex) {customTagName = event.getPlayer().getName();}
 
-										tabPlayer.setValueTemporarily(EnumProperty.CUSTOMTAGNAME, tagPrefix.replace("%eglow_glowcolor%", "") + customTagName);
-										tabPlayer.setValueTemporarily(EnumProperty.TAGPREFIX, color);
-									}
-								} catch (IllegalStateException e) {
-									//Ignored :I
-								}
+								tabPlayer.setValueTemporarily(EnumProperty.CUSTOMTAGNAME, tagPrefix.replace("%eglow_glowcolor%", "") + customTagName);
+								tabPlayer.setValueTemporarily(EnumProperty.TAGPREFIX, color);
 							}
+						} catch (IllegalStateException e) {
+							//Ignored :I
 						}
 					}
-					
-					if (getInstance().getDebugUtil().onBungee())
-						getInstance().getDataManager().TABProxyUpdateRequest(event.getPlayer(), event.getColor());	
-					
-				} catch (ConcurrentModificationException ex2) {
-					//Ignore caused by updating to fast
-				} catch (Exception ex) {
-					ex.printStackTrace();
 				}
 			}
-		}.runTaskAsynchronously(getInstance());		
+			
+			if (getInstance().getDebugUtil().onBungee())
+				getInstance().getDataManager().TABProxyUpdateRequest(event.getPlayer(), event.getColor());	
+			
+		} catch (ConcurrentModificationException ex2) {
+			//Ignore caused by updating to fast
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		/*new BukkitRunnable() {
+			@Override
+			public void run() {
+				
+			}
+		}.runTaskAsynchronously(getInstance());*/
 	}
 	
 	@EventHandler
