71b6661a84ed33b78887a67b6f71f6b667744398
MrGraycat <moorsn@gmail.com>
2022-03-22 07:43:57 +0100

    Added new placeholders

--- src/me/MrGraycat/eGlow/Addon/PlaceholderAPIAddon.java
+++ src/me/MrGraycat/eGlow/Addon/PlaceholderAPIAddon.java
@@ -65,12 +65,28 @@ public class PlaceholderAPIAddon extends PlaceholderExpansion {
         	return "";
         
         switch(identifier.toLowerCase()) {
+        case("client_version"):
+        	return eglowPlayer.getVersion().getFriendlyName();
         case("glowcolor"):
         	return (eglowPlayer.getGlowStatus() && !eglowPlayer.getFakeGlowStatus()) ? eglowPlayer.getActiveColor() + "" : "";
         case("colorchar"):
-        	return (eglowPlayer.getGlowStatus() && !eglowPlayer.getFakeGlowStatus()) ? eglowPlayer.getActiveColor().getChar() + "" : "r";	
+        	return (eglowPlayer.getGlowStatus() && !eglowPlayer.getFakeGlowStatus()) ? eglowPlayer.getActiveColor().getChar() + "" : "r";
+        case("activeglow"):
+        	return (!eglowPlayer.getGlowStatus() && !eglowPlayer.getFakeGlowStatus()) ? Message.COLOR.get("none") : ChatUtil.getEffectChatName(eglowPlayer);
+        case("activeglow_raw"):
+        	return (!eglowPlayer.getGlowStatus() && !eglowPlayer.getFakeGlowStatus()) ? ChatUtil.setToBasicName(Message.COLOR.get("none")) : ChatUtil.setToBasicName(ChatUtil.getEffectChatName(eglowPlayer));
         case("lastglow"):
         	return (eglowPlayer.getLastGlowName());
+        case("lastglow_raw"):
+        	return ChatUtil.setToBasicName(eglowPlayer.getLastGlowName());	
+        case("glowstatus"):
+        	return (eglowPlayer.getGlowStatus() && !eglowPlayer.getFakeGlowStatus()) ? Message.GUI_YES.get() : Message.GUI_NO.get();
+        case("glowstatus_raw"):
+        	return (eglowPlayer.getGlowStatus() && !eglowPlayer.getFakeGlowStatus()) ? "true" : "false";
+        case("glowstatus_join"):
+        	return (eglowPlayer.getGlowOnJoin()) ? Message.GUI_YES.get() : Message.GUI_NO.get();
+        case("glowstatus_join_raw"):
+        	return (eglowPlayer.getGlowOnJoin()) ? "true" : "false";
         case("glow_visibility"):
         	switch (eglowPlayer.getGlowVisibility()) {
 			case UNSUPPORTEDCLIENT:
@@ -78,16 +94,12 @@ public class PlaceholderAPIAddon extends PlaceholderExpansion {
 			default:
 				return Message.valueOf("VISIBILITY_" + eglowPlayer.getGlowVisibility().toString()).get();
 			}
+        
+        //Depreciated placeholders will be removed in a future update
         case("selectedglow"):
-        	return (!eglowPlayer.getGlowStatus() && !eglowPlayer.getFakeGlowStatus()) ? Message.COLOR.get("none") : ChatUtil.getEffectChatName(eglowPlayer);
+        	return "Use %eglow_activeglow%";
         case("selectedglow_raw"):
-        	return (!eglowPlayer.getGlowStatus() && !eglowPlayer.getFakeGlowStatus()) ? ChatUtil.setToBasicName(Message.COLOR.get("none")) : ChatUtil.setToBasicName(ChatUtil.getEffectChatName(eglowPlayer));
-        case("glowstatus"):
-        	return (eglowPlayer.getGlowStatus() && !eglowPlayer.getFakeGlowStatus()) ? Message.GUI_YES.get() : Message.GUI_NO.get();
-        case("glowstatus_join"):
-        	return (eglowPlayer.getGlowOnJoin()) ? Message.GUI_YES.get() : Message.GUI_NO.get();
-        case("glowstatus_raw"):
-        	return String.valueOf(eglowPlayer.getGlowStatus());
+        	return "Use %eglow_activeglow_raw%";
         }
         return null;
     }
