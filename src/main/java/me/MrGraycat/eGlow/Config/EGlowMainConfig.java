73cd9a983180a1cbf3db098a2470b9cd695bcf7a
MrGraycat <moorsn@gmail.com>
2022-06-11 17:27:24 +0200

    Enum references + converter to new config format

--- src/main/java/me/MrGraycat/eGlow/Config/EGlowMainConfig.java
+++ src/main/java/me/MrGraycat/eGlow/Config/EGlowMainConfig.java
@@ -3,6 +3,7 @@ package me.MrGraycat.eGlow.Config;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
@@ -16,9 +17,6 @@ import me.MrGraycat.eGlow.Util.Text.ChatUtil;
 import me.neznamy.yamlassist.YamlAssist;
 
 public class EGlowMainConfig {
-
-	//TODO convert static getters to Enum
-
 	private static YamlConfiguration config;
 	private static File configFile;
 	
@@ -53,7 +51,7 @@ public class EGlowMainConfig {
 				configFile.renameTo(oldFile);
 				initialize();
 			}
-			
+
 			configCheck();
 		} catch(Exception e) {
 			ChatUtil.reportError(e);
@@ -97,232 +95,191 @@ public class EGlowMainConfig {
 	}
 	
 	private static void configCheck() {
-		addIfMissing("Tabname.Enable", false);
-		addIfMissing("Tabname.tabPrefix", "%prefix%");
-		addIfMissing("Tabname.tabName", "&r%name%");
-		addIfMissing("Tabname.tabSuffix", "%suffix%");
-		
-		addIfMissing("Tagname.Enable", false);
-		addIfMissing("Tagname.tagPrefix", "%prefix%");
-		addIfMissing("Tagname.tagSuffix", "%suffix%");
-		addIfMissing("Force-glow.Bypass-blocked-worlds", false);
-		addIfMissing("Options.Send-invisibility-notification", true);
-		addIfMissing("Options.Advanced-TAB-integration", false);
-		addIfMissing("Options.Disable-glow-when-invisible", true);
-		addIfMissing("Options.Mention-glow-state-on-join", false);
-		addIfMissing("Actionbars.Enable", true);
-		addIfMissing("Actionbars.Use-in-GUI", false);
+		replaceOrAdd("Actionbars.Enable", "Actionbars.enable", true);
+		replaceOrAdd("Actionbars.Use-in-GUI","Actionbars.use-in-GUI", false);
+
+		replaceOrAdd("Command-alias.Enable", "Command-alias.enable", false);
+		replaceOrAdd("Command-alias.Alias", "Command-alias.alias", "glow");
+
+		replaceOrAdd("Delays.Player.Slow", "Delays.slow", 2.0);
+		replaceOrAdd("Delays.Player.Fast", "Delays.fast", 1.0);
+
+		replaceOrAdd("Tabname.Enable", "Formatting.tablist.enable", false);
+		replaceOrAdd("NA", "Formatting.tablist.format", "%prefix% &r%name% %suffix%");
+		replaceOrAdd("Tagname.Enable", "Formatting.tagname.enable", false);
+		replaceOrAdd("Tagname.tagPrefix", "Formatting.tagname.prefix", "%prefix%");
+		replaceOrAdd("Tagname.tagSuffix", "Formatting.tagname.suffix", "%suffix%");
+
+		replaceOrAdd("MySQL.Enable", "MySQL.enable", false);
+		replaceOrAdd("MySQL.Host", "MySQL.host", "localhost");
+		replaceOrAdd("MySQL.Port", "MySQL.port", 3306);
+		replaceOrAdd("NA", "MySQL.DBName", "");
+		replaceOrAdd("MySQL.Username", "MySQL.username", "root");
+		replaceOrAdd("MySQL.Password", "MySQL.password", "123");
+
+		replaceOrAdd("World.Enable", "World.enable", false);
+		replaceOrAdd("World.Action", "World.action", "BLOCK");
+		replaceOrAdd("World.Worlds", "World.worlds", Arrays.asList("world1", "world2"));
+
+		replaceOrAdd("Options.Disable-glow-when-invisible", "Settings.disable-glow-when-invisible", true);
+		replaceOrAdd("Options.Advanced-TAB-integration", "Settings.smart-TAB-nametag-handler", false);
+
+		replaceOrAdd("Options.Render-player-skulls", "Settings.gui.render-skulls", true);
+		replaceOrAdd("Options.Inventory-add-glass", "Settings.gui.add-glass-panes", true);
+		replaceOrAdd("Options.Disable-prefix-in-GUI", "Settings.gui.add-prefix-to-title", true);
+		replaceOrAdd("Options.Use-GUI-color-as-chat-color", "Settings.gui.use-gui-color-for-messages", false);
+		replaceOrAdd("NA", "Settings.gui.max-personal-effect-size", 10);
+
+		replaceOrAdd("Options.PermissionCheck-on-join", "Settings.join.check-glow-permission", false);
+		replaceOrAdd("Options.Default-glow-on-join-value", "Settings.join.default-glow-on-join-value", true);
+		replaceOrAdd("Options.Mention-glow-state-on-join", "Settings.join.mention-glow-state", false);
+		replaceOrAdd("Force-glow.Enable", "Settings.join.force-glows.enable", false);
+		replaceOrAdd("Force-glow.Bypass-blocked-worlds", "Settings.join.force-glows.bypass-blocked-worlds", false);
+		replaceOrAdd("Force-glow.Glows", "Settings.join.force-glows.glows", true);
+		replaceOrAdd("Options.Send-update-notifications", "Settings.notifications.plugin-update", true);
+		replaceOrAdd("Options.Send-invisibility-notification", "Settings.notifications.invisibility-change", true);
+		replaceOrAdd("Options.Send-target-notification", "Settings.notifications.target-set-unset-command", true);
+
+		replaceOrAdd("MySQL.useSSL", "Advanced.MySQL.useSSL", true);
+		replaceOrAdd("Options.Collision", "Advanced.teams.entity-collision", true);
+		replaceOrAdd("Options.Nametag", "Advanced.teams.nametag-visibility", true);
+		replaceOrAdd("Options.Remove-scoreboard-on-join", "Advanced.teams.remove-teams-on-join", true);
+		replaceOrAdd("Options.Feature-team-packets", "Advanced.teams.send-eGlow-team-packets", true);
+		replaceOrAdd("Options.Feature-packet-blocker", "Advanced.packets.smart-packet-blocker", true);
+		remove( "Delays.Player", "Tabname", "Tabname.tabPrefix", "Tabname.tabName", "Tabname.tabSuffix", "Tagname", "Force-glow", "Options");
 	}
-	
-	private static void addIfMissing(String path, Object value) {
+
+	public enum MainConfig {
+		ACTIONBARS_ENABLE("Actionbars.enable"),
+		ACTIONBARS_IN_GUI("Actionbars.use-in-GUI"),
+
+		COMMAND_ALIAS_ENABLE("Command-alias.enable"),
+		COMMAND_ALIAS("Command-alias.alias"),
+
+		DELAY_SLOW("Delays.slow"),
+		DELAY_FAST("Delays.fast"),
+
+		FORMATTING_TABLIST_ENABLE("Formatting.tablist.enable"),
+		FORMATTING_TABLIST_FORMAT("Formatting.tablist.format"),
+		FORMATTING_TAGNAME_ENABLE("Formatting.tagname.enable"),
+		FORMATTING_TAGNAME_PREFIX("Formatting.tagname.prefix"),
+		FORMATTING_TAGNAME_SUFFIX("Formatting.tagname.suffix"),
+
+		MYSQL_ENABLE("MySQL.enable"),
+		MYSQL_HOST("MySQL.host"),
+		MYSQL_PORT("MySQL.port"),
+		MYSQL_DBNAME("MySQL.DBName"),
+		MYSQL_USERNAME("MySQL.username"),
+		MYSQL_PASSWORD("MySQL.password"),
+
+		WORLD_ENABLE("World.enable"),
+		WORLD_ACTION("World.action"),
+		WORLD_LIST("World.worlds"),
+
+		SETTINGS_DISABLE_GLOW_WHEN_INVISIBLE("Settings.disable-glow-when-invisible"),
+		SETTINGS_SMART_TAB_NAMETAG_HANDLER("Settings.smart-TAB-nametag-handler"),
+
+		SETTINGS_GUI_RENDER_SKULLS("Settings.gui.render-skulls"),
+		SETTINGS_GUI_ADD_GLASS_PANES("Settings.gui.add-glass-panes"),
+		SETTINGS_GUI_ADD_PREFIX("Settings.gui.add-prefix-to-title"),
+		SETTINGS_GUI_COLOR_FOR_MESSAGES("Settings.gui.use-gui-color-for-messages"),
+		SETTINGS_GUI_MAX_PERSONAL_GLOW_SIZE("Settings.gui.max-personal-effect-size"),
+
+		SETTINGS_JOIN_CHECK_PERMISSION("Settings.join.check-glow-permission"),
+		SETTINGS_JOIN_DEFAULT_GLOW_ON_JOIN_VALUE("Settings.join.default-glow-on-join-value"),
+		SETTINGS_JOIN_MENTION_GLOW_STATE("Settings.join.mention-glow-state"),
+		SETTINGS_JOIN_FORCE_GLOWS_ENABLE("Settings.join.force-glows.enable"),
+		SETTINGS_JOIN_FORCE_GLOWS_BYPASS_BLOCKED_WORLDS("Settings.join.force-glows.bypass-blocked-worlds"),
+		SETTINGS_JOIN_FORCE_GLOWS_LIST("Settings.join.force-glows.glows"),
+
+		SETTINGS_NOTIFICATIONS_UPDATE("Settings.notifications.plugin-update"),
+		SETTINGS_NOTIFICATIONS_INVISIBILITY("Settings.notifications.invisibility-change"),
+		SETTINGS_NOTIFICATIONS_TARGET_COMMAND("Settings.notifications.target-set-unset-command"),
+
+		ADVANCED_MYSQL_USESSL("Advanced.MySQL.useSSL"),
+
+		ADVANCED_TEAMS_ENTITY_COLLISION("Advanced.Teams.entity-collision"),
+		ADVANCED_TEAMS_NAMETAG_VISIBILITY("Advanced.Teams.nametag-visibility"),
+		ADVANCED_TEAMS_REMOVE_ON_JOIN("Advanced.Teams.remove-teams-on-join"),
+		ADVANCED_TEAMS_SEND_PACKETS("Advanced.Teams.send-eGlow-team-packets"),
+
+		ADVANCED_PACKETS_SMART_BLOCKER("Advanced.Packets.smart-packet-blocker");
+
+		private final MainConfig main;
+		private final String configPath;
+
+		MainConfig(String configPath) {
+			this.main = this;
+			this.configPath = configPath;
+		}
+
+		public String getConfigPath() {
+			return configPath;
+		}
+
+		public String getString() {
+			return config.getString(main.getConfigPath());
+		}
+
+		public String getString(String name) {
+			return config.getString(main.getConfigPath() + "." + name);
+		}
+
+		public List<String> getStringList() {
+			return config.getStringList(main.getConfigPath());
+		}
+
+		public int getInt() {
+			switch (main) {
+				case DELAY_SLOW:
+				case DELAY_FAST:
+					return (int) (config.getDouble(main.getConfigPath()) * 20);
+				default:
+					return config.getInt(main.getConfigPath());
+			}
+		}
+
+		public Boolean getBoolean() {
+			return config.getBoolean(main.getConfigPath());
+		}
+
+		public Set<String> getConfigSection() {
+			return config.getConfigurationSection(main.getConfigPath()).getKeys(false);
+		}
+	}
+
+	private static void replaceOrAdd(String oldPath, String newPath, Object value) {
 		try {
-			if (!config.contains(path)) {
-				config.set(path, value);
-				config.save(configFile);
-			}	
+			if (config.contains(oldPath)) {
+				config.set(newPath, config.get(oldPath));
+				config.set(oldPath, null);
+			} else if (!config.contains(newPath)) {
+				config.set(newPath, value);
+			}
+
+			config.save(configFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void remove(String... paths) {
+		try {
+			for (String path : paths) {
+				config.set(path, null);
+			}
+
+			config.save(configFile);
 		} catch (Exception e) {
 			ChatUtil.reportError(e);
 		}
 	}
 	
-	public static boolean getWorldCheckEnabled() {
-		return config.getBoolean("World.Enable");
-	}
-	
-	public static GlowWorldAction getWorldAction() {
-		try {
-			return GlowWorldAction.valueOf(config.getString("World.Action").toUpperCase() + "ED");
-		} catch (IllegalArgumentException e) {
-			return GlowWorldAction.UNKNOWN;
-		}
-	}
-	
-	public static List<String> getWorlds() {
-		List<String> worlds = new ArrayList<>();
-		
-		for (String world : config.getStringList("World.Worlds")) {
-			worlds.add(world.toLowerCase());
-		}		
-		return worlds;
-	}
-	
-	public static boolean getForceGlowEnabled() {
-		return config.getBoolean("Force-glow.Enable");
-	}
-	
-	public static boolean getForceGlowBypassBlockedWorlds() {
-		return config.getBoolean("Force-glow.Bypass-blocked-worlds");
-	}
-	
-	public static Set<String> getForceGlowList() {
-		return config.getConfigurationSection("Force-glow.Glows").getKeys(false);
-	}
-	
-	public static String getForceGlowEffect(String name) {
-		return config.getString("Force-glow.Glows." + name);
-	}
-	
-	public static int getPlayerSlowDelay() {
-		return (int) (config.getDouble("Delays.Player.Slow") * 20);
-	}
-	
-	public static int getPlayerFastDelay() {
-		return (int) (config.getDouble("Delays.Player.Fast") * 20);
-	}
-	
-	/*public static int getNPCSlowDelay() {
-		return (int) (config.getDouble("Delays.NPC.Slow") * 20);
-	}
-
-	public static int getNPCFastDelay() {
-		return (int) (config.getDouble("Delays.NPC.Fast") * 20);
-	}*/
-
-	public static boolean OptionEnableCommandAlias() {
-		return config.getBoolean("Command-alias.Enable");
-	}
-	
-	public static String OptionCommandAlias() {
-		return config.getString("Command-alias.Alias");
-	}
-
-	public static boolean EnableActionbarMessages() {
-		return config.getBoolean("Actionbars.Enable");
-	}
-
-	public static boolean UseActionbarInGUI() {
-		return config.getBoolean("Actionbars.Use-in-GUI");
-	}
-
-	public static boolean OptionDisableGlowWhenInvisible() {
-		return config.getBoolean("Options.Disable-glow-when-invisible");
-	}
-	
-	public static boolean OptionSendInvisibilityNotification() {
-		return config.getBoolean("Options.Send-invisibility-notification");
-	}
-	
-	public static boolean OptionRenderPlayerSkulls() {
-		return config.getBoolean("Options.Render-player-skulls");
-	}
-	
-	public static boolean OptionDefaultGlowOnJoinValue() {
-		return config.getBoolean("Options.Default-glow-on-join-value");
-	}
-	
-	public static boolean OptionSendTargetNotification() {
-		return config.getBoolean("Options.Send-target-notification");
-	}
-	
-	public static boolean OptionPermissionCheckonJoin() {
-		return config.getBoolean("Options.PermissionCheck-on-join");
-	}
-	
-	public static boolean OptionSendUpdateNotifications() {
-		return config.getBoolean("Options.Send-update-notifications");
-	}	
-	
-	public static boolean OptionAdvancedTABIntegration() {
-		return config.contains("Options.Advanced-TAB-integration") && config.getBoolean("Options.Advanced-TAB-integration");
-	}
-
-	public static boolean useMySQL() {
-		return config.getBoolean("MySQL.Enable");
-	}
-	
-	public static String getMySQLHost() {
-		return config.getString("MySQL.Host");
-	}
-	
-	public static int getMySQLPort() {
-		return config.getInt("MySQL.Port");
-	}
-	
-	public static String getMySQLDBName() {
-		return config.getString("MySQL.DBName");
-	}
-	
-	public static String getMySQLUsername() {
-		return config.getString("MySQL.Username");
-	}
-	
-	public static String getMySQLPassword( ) {
-		return config.getString("MySQL.Password");
-	}
-	
-	public static boolean getMySQLUseSSL() {
-		return !config.contains("MySQL.useSSL") || config.getBoolean("MySQL.useSSL");
-	}
-	
-	public static boolean setTabnameFormat() {
-		return config.getBoolean("Tabname.Enable");
-	}
-	
-	public static String getTabPrefix() {
-		return config.getString("Tabname.tabPrefix");
-	}
-	
-	public static String getTabName() {
-		return config.getString("Tabname.tabName");
-	}
-	
-	public static String getTabSuffix() {
-		return config.getString("Tabname.tabSuffix");
-	}
-	
-	public static boolean setTagnameFormat() {
-		return config.getBoolean("Tagname.Enable");
-	}
-	
-	public static String getTagPrefix() {
-		return config.getString("Tagname.tagPrefix");
-	}
-	
-	public static String getTagSuffix() {
-		return config.getString("Tagname.tagSuffix");
-	}
-	
-	//Hidden
-	public static boolean OptionDoTeamCollision() {
-		return !config.contains("Options.Collision") || config.getBoolean("Options.Collision");
-	}
-	public static boolean OptionShowNametag() {
-		return !config.contains("Options.Nametag") || config.getBoolean("Options.Nametag");
-	}
-	
-	public static boolean OptionUseGUIColorAsChatColor() {
-		return config.contains("Options.Use-GUI-color-as-chat-color") && config.getBoolean("Options.Use-GUI-color-as-chat-color");
-	}
-	
-	public static boolean OptionAddGlassToInv() {
-		return !config.contains("Options.Inventory-add-glass") || config.getBoolean("Options.Inventory-add-glass");
-	}
-	
-	public static boolean OptionRemoveScoreboardOnJoin() {
-		return !config.contains("Options.Remove-scoreboard-on-join") || config.getBoolean("Options.Remove-scoreboard-on-join");
-	}
-	
-	public static boolean OptionFeaturePacketBlocker() {
-		return !config.contains("Options.Feature-packet-blocker") || config.getBoolean("Options.Feature-packet-blocker");
-	}
-	
-	public static boolean OptionFeatureTeamPackets() {
-		return !config.contains("Options.Feature-team-packets") || config.getBoolean("Options.Feature-team-packets");
-	}
-	
-	public static boolean OptionDisablePrefixInGUI() {
-		return config.contains("Options.Disable-prefix-in-GUI") && config.getBoolean("Options.Disable-prefix-in-GUI");
-	}
-	
-	public static boolean OptionMentionGlowState() {
-		return config.contains("Options.Mention-glow-state-on-join") && config.getBoolean("Options.Mention-glow-state-on-join");
-	}
-	
 	private static void registerCustomPermissions() {
-		if (!getForceGlowEnabled())
+		if (!MainConfig.SETTINGS_JOIN_FORCE_GLOWS_ENABLE.getBoolean())
 			return;
 		
-		for (String name : getForceGlowList()) {
+		for (String name : MainConfig.SETTINGS_JOIN_FORCE_GLOWS_LIST.getConfigSection()) {
 			try {
 				EGlow.getInstance().getServer().getPluginManager().addPermission(new Permission("eglow.force." + name.toLowerCase()));
 			} catch (Exception ignored) {}
