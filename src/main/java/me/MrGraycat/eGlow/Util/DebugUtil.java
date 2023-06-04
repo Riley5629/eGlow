b4762c66a8704f766bd0b9f842aa0f4c1ae727fc
MrGraycat <moorsn@gmail.com>
2023-06-04 11:35:29 +0200

    Fix - EGlow still sending packets when it shoudn't

--- src/main/java/me/MrGraycat/eGlow/Util/DebugUtil.java
+++ src/main/java/me/MrGraycat/eGlow/Util/DebugUtil.java
@@ -13,9 +13,9 @@ import java.util.Objects;
 
 public class DebugUtil {
 	private static final String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
-	private static final int minorVersion =  Integer.parseInt(version.split("_")[1]);
+	private static final int minorVersion = Integer.parseInt(version.split("_")[1]);
 	private static final PluginManager pm = Bukkit.getPluginManager();
-	
+
 	public static void sendDebug(CommandSender sender, IEGlowPlayer ePlayer) {
 		StringBuilder plugins = new StringBuilder(" ");
 
@@ -30,65 +30,65 @@ public class DebugUtil {
 			ChatUtil.sendPlainMsg(sender, "  &fForced glow: " + ((ePlayer.getForceGlow() == null) ? "&eNone" : ePlayer.getForceGlow().getName()), false);
 			ChatUtil.sendPlainMsg(sender, "  &fGlow blocked reason: &e" + ePlayer.getGlowDisableReason(), false);
 		}
-		
+
 		ChatUtil.sendPlainMsg(sender, "&f&m                                                                               ", false);
 		ChatUtil.sendPlainMsg(sender, "&fServer version: &e" + version, false);
 		ChatUtil.sendPlainMsg(sender, "Plugins:", false);
 		for (Plugin plugin : Bukkit.getPluginManager().getPlugins()) {
 			String pluginName = plugin.getDescription().getName();
-			
+
 			if (plugin.isEnabled()) {
 				String pluginText = (pluginName.equalsIgnoreCase("eGlow") || pluginName.equalsIgnoreCase("TAB")) ? "&6" + pluginName + " &f(" + plugin.getDescription().getVersion() + "), " : "&a" + pluginName + "&f, ";
-				
+
 				plugins.append(pluginText);
 			} else {
 				plugins.append("&c").append(pluginName).append("&f, ");
 			}
 		}
-		
-		
+
+
 		ChatUtil.sendPlainMsg(sender, ChatUtil.translateColors(plugins.substring(0, plugins.length() - 2)), false);
 
 		if (EGlow.getInstance().getTABAddon() != null && !EGlow.getInstance().getTABAddon().getTABSupported())
 			ChatUtil.sendPlainMsg(sender, ChatUtil.translateColors("&cThis eGlow version requires a minimum TAB version of 3.1.0&f!"), false);
 	}
-	
+
 	public static String getServerVersion() {
 		return version;
 	}
-	
+
 	public static int getMinorVersion() {
 		return minorVersion;
 	}
-	
+
 	public static boolean isProtocolSupportInstalled() {
 		return pluginCheck("ProtocolSupport");
 	}
-	
+
 	public static boolean isViaVersionInstalled() {
 		return pluginCheck("ViaVersion");
 	}
 
 	public static boolean isTABBridgeInstalled() {
-		return pluginCheck("TAB-Bridge");
+		return (EGlow.getInstance().getTABAddon() == null && pluginCheck("TAB-Bridge"));
 	}
 
 	public static boolean onBungee() {
 		return !Bukkit.getServer().getOnlineMode() && NMSHook.isBungee();
 	}
-	
+
 	public static boolean pluginCheck(String plugin) {
 		return pm.getPlugin(plugin) != null && Objects.requireNonNull(pm.getPlugin(plugin)).isEnabled();
 	}
-	
+
 	public static Plugin getPlugin(String plugin) {
 		return pm.getPlugin(plugin);
 	}
-	
+
 	public static boolean TABInstalled() {
 		return (pluginCheck("TAB") && getPlugin("TAB").getClass().getName().startsWith("me.neznamy.tab"));
 	}
-	
+
 	public static boolean isPAPIInstalled() {
 		return pluginCheck("PlaceholderAPI");
 	}
