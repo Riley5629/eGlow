98aeaa5bf7bd215bce96cbbf681a228cb73e52b4
MrGraycat <moorsn@gmail.com>
2023-06-08 06:37:45 +0200

    Formatting

--- src/main/java/me/MrGraycat/eGlow/Addon/GSitAddon.java
+++ src/main/java/me/MrGraycat/eGlow/Addon/GSitAddon.java
@@ -17,57 +17,57 @@ import java.util.ArrayList;
 
 public class GSitAddon implements Listener {
 
-    static ArrayList<Player> activePlayers = new ArrayList<>();
+	static ArrayList<Player> activePlayers = new ArrayList<>();
 
-    public GSitAddon() {
-        EGlow.getInstance().getServer().getPluginManager().registerEvents(this, EGlow.getInstance());
-    }
+	public GSitAddon() {
+		EGlow.getInstance().getServer().getPluginManager().registerEvents(this, EGlow.getInstance());
+	}
 
-    @EventHandler
-    public void poseEvent(PlayerPoseEvent event) {
-        Player player = event.getPlayer();
-        checkGlow(player, true);
-    }
+	@EventHandler
+	public void poseEvent(PlayerPoseEvent event) {
+		Player player = event.getPlayer();
+		checkGlow(player, true);
+	}
 
-    @EventHandler
-    public void unPoseEvent(PlayerGetUpPoseEvent event) {
-        Player player = event.getPlayer();
-        checkGlow(player, false);
-    }
+	@EventHandler
+	public void unPoseEvent(PlayerGetUpPoseEvent event) {
+		Player player = event.getPlayer();
+		checkGlow(player, false);
+	}
 
-    @EventHandler
-    public void onDisconnect(PlayerQuitEvent event) {
-        checkGlow(event.getPlayer(), false);
-    }
+	@EventHandler
+	public void onDisconnect(PlayerQuitEvent event) {
+		checkGlow(event.getPlayer(), false);
+	}
 
-    @EventHandler
-    public void onGlowChange(GlowColorChangeEvent event) {
-        Player player = event.getPlayer();
+	@EventHandler
+	public void onGlowChange(GlowColorChangeEvent event) {
+		Player player = event.getPlayer();
 
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                if (GSitAPI.isPosing(player)) {
-                    IEGlowPlayer ePlayer = DataManager.getEGlowPlayer(player);
-                    ePlayer.disableGlow(false);
-                }
-            }
-        }.runTaskLater(EGlow.getInstance(), 2L);
-    }
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				if (GSitAPI.isPosing(player)) {
+					IEGlowPlayer ePlayer = DataManager.getEGlowPlayer(player);
+					ePlayer.disableGlow(false);
+				}
+			}
+		}.runTaskLater(EGlow.getInstance(), 2L);
+	}
 
-    private void checkGlow(Player player, boolean posing) {
-        IEGlowPlayer ePlayer = DataManager.getEGlowPlayer(player);
+	private void checkGlow(Player player, boolean posing) {
+		IEGlowPlayer ePlayer = DataManager.getEGlowPlayer(player);
 
-        if (posing) {
-            if (ePlayer.getGlowStatus() || ePlayer.getFakeGlowStatus()) {
-                if (!activePlayers.contains(player)) activePlayers.add(player);
-                ePlayer.disableGlow(false);
-            }
-        } else {
-            if (activePlayers.contains(player)) {
-                activePlayers.remove(player);
-                ePlayer.activateGlow();
-            }
-        }
-    }
+		if (posing) {
+			if (ePlayer.getGlowStatus() || ePlayer.getFakeGlowStatus()) {
+				if (!activePlayers.contains(player)) activePlayers.add(player);
+				ePlayer.disableGlow(false);
+			}
+		} else {
+			if (activePlayers.contains(player)) {
+				activePlayers.remove(player);
+				ePlayer.activateGlow();
+			}
+		}
+	}
 }
