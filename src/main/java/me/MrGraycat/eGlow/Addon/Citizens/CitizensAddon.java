f983f512326c8385a254afd2957ff7c3b342529f
MrGraycat <moorsn@gmail.com>
2023-06-08 21:15:29 +0200

    Cleanup - Citizens addon

--- src/main/java/me/MrGraycat/eGlow/Addon/Citizens/CitizensAddon.java
+++ src/main/java/me/MrGraycat/eGlow/Addon/Citizens/CitizensAddon.java
@@ -8,30 +8,31 @@ import net.citizensnpcs.trait.ScoreboardTrait;
 public class CitizensAddon {
 	/**
 	 * Loads in the custom EGlow trait for citizen NPC's
-	 * @exception IllegalArgumentException thrown when the trait is already registered (Ignored)
+	 *
+	 * @throws IllegalArgumentException thrown when the trait is already registered (Ignored)
 	 */
 	public CitizensAddon() {
 		try {
 			CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(EGlowCitizensTrait.class).withName("eGlow"));
-		} catch (IllegalArgumentException e) {
-			//Trait is already registered
+		} catch (IllegalArgumentException ignored) {
 		}
 	}
 
 	/**
 	 * Check to see if required traits exist & were successfully applied to the NPC
+	 *
 	 * @param npc Citizens NPC
 	 * @return true if required traits exist and are applied, false if not
-	 * @exception NoClassDefFoundError thrown when using an old version of Citizens where the scoreboardTrait doesn't exist
+	 * @throws NoClassDefFoundError thrown when using an old version of Citizens where the scoreboardTrait doesn't exist
 	 */
 	public boolean traitCheck(NPC npc) {
 		try {
-			if (!npc.hasTrait(ScoreboardTrait.class)) 
+			if (!npc.hasTrait(ScoreboardTrait.class))
 				npc.addTrait(ScoreboardTrait.class);
-		} catch (NoClassDefFoundError e) {
+		} catch (NoClassDefFoundError ignored) {
 			return false;
 		}
-		
+
 		if (!npc.hasTrait(EGlowCitizensTrait.class))
 			npc.addTrait(EGlowCitizensTrait.class);
 		return true;
--- src/main/java/me/MrGraycat/eGlow/Addon/Citizens/EGlowCitizensTrait.java
+++ src/main/java/me/MrGraycat/eGlow/Addon/Citizens/EGlowCitizensTrait.java
@@ -1,5 +1,7 @@
 package me.MrGraycat.eGlow.Addon.Citizens;
 
+import lombok.Getter;
+import lombok.Setter;
 import me.MrGraycat.eGlow.Manager.Interface.IEGlowPlayer;
 import me.MrGraycat.eGlow.Util.Text.ChatUtil;
 import net.citizensnpcs.api.persistence.Persist;
@@ -7,58 +9,45 @@ import net.citizensnpcs.api.trait.Trait;
 import net.citizensnpcs.api.util.DataKey;
 
 public class EGlowCitizensTrait extends Trait {
+	@Getter
+	@Setter
 	IEGlowPlayer eGlowNPC = null;
-	
+
+	@Getter
+	@Setter
 	@Persist("LastEffect")
 	String lastEffect = "none";
-	
+
 	public EGlowCitizensTrait() {
 		super("eGlow");
 	}
-	
+
+	@Override
 	public void load(DataKey key) {
 		setLastEffect(key.getString("LastEffect", "none"));
 	}
-	
+
+	@Override
 	public void save(DataKey key) {
 		if (getEGlowNPC() != null) {
 			key.setString("LastEffect", (getEGlowNPC().isGlowing()) ? getEGlowNPC().getEffect().getName() : "none");
 		}
 	}
-	
+
+	@Override
 	public void onSpawn() {
 		if (getEGlowNPC() == null) {
-			setEGlowNPC(new IEGlowPlayer(npc));
+			setEGlowNPC(new IEGlowPlayer(this.npc));
 		}
 
 		getEGlowNPC().disableGlow(true);
 		getEGlowNPC().setDataFromLastGlow(getLastEffect());
-		
+
 		try {
-			if (!npc.getOrAddTrait(EGlowCitizensTrait.class).getLastEffect().equals("none"))
+			if (!this.npc.getOrAddTrait(EGlowCitizensTrait.class).getLastEffect().equals("none"))
 				getEGlowNPC().activateGlow();
-		} catch(NoSuchMethodError e) {
+		} catch (NoSuchMethodError ignored) {
 			ChatUtil.sendToConsole("&cYour Citizens version is outdated please update it", true);
 		}
-	}
-	
-	public void onDespawn() {}
-
-	public void onRemove() {}
-	
-	private void setEGlowNPC(IEGlowPlayer entity) {
-		this.eGlowNPC = entity;
-	}
-	
-	public IEGlowPlayer getEGlowNPC() {
-		return this.eGlowNPC;
-	}
-	
-	private void setLastEffect(String lastEffect) {
-		this.lastEffect = lastEffect;
-	}
-	
-	private String getLastEffect() {
-		return this.lastEffect;
 	}
 }
